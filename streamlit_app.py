8ccca2074730ae482927d4d4eae99c26eec96030
Thackattack10 <50476366+Thackattack10@users.noreply.github.com>
2025-07-28 15:08:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,17 @@
+from lineups import scrape_lineups
 import streamlit as st
 from scrape import load_fanduel_salaries
 from projections import project_points
+lineup_df = scrape_lineups()
+df['on_PP1'] = df['Nickname'].isin(
+    lineup_df[(lineup_df.LineType == 'PP') & (lineup_df.LineNumber == '1')]['Player']
+)
+df['on_EV1'] = df['Nickname'].isin(
+    lineup_df[(lineup_df.LineType == 'EV') & (lineup_df.LineNumber == '1')]['Player']
+)
 from optimizer import optimize_lineup
-
+st.subheader("🧩 Line Combinations (DailyFaceoff)")
+st.dataframe(lineup_df)
 st.title("🚨 NHL FanDuel DFS Optimizer")
 
 salary_file = st.file_uploader("Upload FanDuel CSV", type="csv")
